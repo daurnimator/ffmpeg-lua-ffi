700b225e6e668c6a3ed6a526c8b09421a48cb71e
daurnimator <quae@daurnimator.com>
2011-10-17 23:59:21 +1100

    Move from lomp2 repository

---
+++ ffmpeg_test.lua
@@ -0,0 +1,69 @@
+package.path = package.path .. ";./?/init.lua"
+
+-- Based on https://github.com/mkottman/ffi_fun/blob/master/ffmpeg_audio.lua
+
+local assert , ipairs , tonumber = assert , ipairs , tonumber
+local ioopen = io.open
+
+local ffi = 		require"ffi"
+local ffmpeg = 		require"ffmpeg"
+local avutil = 		ffmpeg.avutil
+local avAssert = 	ffmpeg.avAssert
+local avcodec = 	ffmpeg.avcodec
+local avformat =	ffmpeg.avformat
+
+
+local FILENAME = arg[1] or 'song.mp3'
+local SECTION = print
+
+
+SECTION "Opening file"
+
+local formatctx = ffmpeg.openfile ( FILENAME )
+local audioctx = assert ( ffmpeg.findaudiostreams ( formatctx ) [ 1 ] , "No Audio Stream Found" )
+
+print ( "Bitrate:", 	tonumber(audioctx.bit_rate))
+print ( "Channels:", 	tonumber(audioctx.channels))
+print ( "Sample rate:",	tonumber(audioctx.sample_rate))
+print ( "Sample type:",	({[0]="u8", "s16", "s32", "flt", "dbl"}) [ audioctx.sample_fmt ] )
+
+
+SECTION "Decoding"
+
+local all_samples = {}
+local total_samples = 0
+
+local buffsize = 192000--ffmpeg.AVCODEC_MAX_AUDIO_FRAME_SIZE
+local frame_size = ffi.new("int[1]")
+
+local output_type = ffmpeg.format_to_type [ audioctx.sample_fmt ]
+local output_buff = ffi.new ( output_type .. "[?]" , buffsize )
+for packet in ffmpeg.read_frames ( formatctx ) do
+	frame_size[0] = buffsize
+	avAssert ( avcodec.avcodec_decode_audio3 ( audioctx , output_buff , frame_size , packet ) )
+	local size = tonumber ( frame_size[0] ) / ffi.sizeof ( output_type ) -- frame_size is in bytes
+
+	local frame = ffi.new("int16_t[?]", size)
+	ffi.copy ( frame , output_buff , size*2 )
+	all_samples[#all_samples + 1] = frame
+	total_samples = total_samples + size
+end
+
+
+SECTION "Merging samples"
+
+local samples = ffi.new ( "int16_t[?]" , total_samples )
+local offset = 0
+for _ , s in ipairs ( all_samples ) do
+	local size = ffi.sizeof ( s )
+	ffi.copy ( samples + offset , s , size )
+	offset = offset + size/2
+end
+
+
+SECTION "Generating 'samples.raw'"
+
+local out = assert ( ioopen ( 'samples.raw' , 'wb' ) )
+local size = ffi.sizeof ( samples )
+out:write ( ffi.string ( samples , size ) )
+out:close ( )
---
+++ init.lua
@@ -0,0 +1,115 @@
+local rel_dir = ...
+
+-- FFI binding to FFmpeg
+local path_to_include = rel_dir .. "/include/"
+
+local assert , error = assert , error
+local setmetatable = setmetatable
+local tonumber , tostring = tonumber , tostring
+local tblinsert = table.insert
+
+-- Windows binaries from http://ffmpeg.zeranoe.com/builds/
+
+local ffi = require"ffi"
+local ffi_util = require"ffi_util"
+local ffi_add_include_dir 	= ffi_util.ffi_add_include_dir
+local ffi_defs 				= ffi_util.ffi_defs
+local ffi_process_defines 	= ffi_util.ffi_process_defines
+
+ffi_add_include_dir ( path_to_include )
+
+ffi_defs ( rel_dir .. "/defs.h" , { --TODO: remove rel_dir
+		[[libavutil/avstring.h]] ;
+		[[libavcodec/avcodec.h]] ;
+		[[libavformat/avformat.h]] ;
+	} )
+
+local avutil , avcodec , avformat
+if jit.os == "Windows" then
+	avutil 		= ffi.load ( rel_dir .. [[/avutil-51]] )
+	avcodec 	= ffi.load ( rel_dir .. [[/avcodec-53]] )
+	avformat 	= ffi.load ( rel_dir .. [[/avformat-53]] )
+elseif jit.os == "Linux" or jit.os == "OSX" or jit.os == "POSIX" or jit.os == "BSD" then
+	avutil 		= ffi.load ( [[libavutil]] )
+	avcodec 	= ffi.load ( [[libavcodec]] )
+	avformat 	= ffi.load ( [[libavformat]] )
+else
+	error("Unknown platform")
+end
+
+local ffmpeg = {
+	avutil = avutil ;
+	avcodec = avcodec ;
+	avformat = avformat ;
+}
+
+ffmpeg.format_to_type = {
+	--[avutil.AV_SAMPLE_FMT_NONE] 	= nil ;
+	[avutil.AV_SAMPLE_FMT_U8] 	= "uint8_t" ;
+	[avutil.AV_SAMPLE_FMT_S16] 	= "int16_t" ;
+	[avutil.AV_SAMPLE_FMT_S32] 	= "int32_t" ;
+	[avutil.AV_SAMPLE_FMT_FLT] 	= "float" ;
+	[avutil.AV_SAMPLE_FMT_DBL] 	= "double" ;
+	--[avutil.AV_SAMPLE_FMT_NB] 	= "" ;
+}
+
+
+local function avAssert ( err )
+	if err < 0 then
+		local errbuff = ffi.new ( "uint8_t[256]" )
+		local ret = avutil.av_strerror ( err , errbuf , 256 )
+		if ret ~= -1 then
+			error ( ffi.string ( errbuf ) , 2 )
+		else
+			error ( 'Unknown AV error: ' .. tostring ( ret ) , 2 )
+		end
+	end
+	return err
+end
+ffmpeg.avAssert = avAssert
+
+local formatcontext_mt_openfile = {
+	__gc = function ( o ) return avformat.av_close_input_stream ( o.context ) end ;
+}
+function ffmpeg.openfile ( file )
+	assert ( file , "No input file" )
+	local formatContext = ffi.new ( "AVFormatContext*[1]")
+	avAssert(avformat.avformat_open_input ( formatContext , file , nil , nil ))
+	return setmetatable ( { context = formatContext[0] } , formatcontext_mt_openfile )
+end
+
+function ffmpeg.findaudiostreams ( formatContext )
+	formatContext = formatContext.context
+	avAssert( avformat.av_find_stream_info ( formatContext ) )
+
+	local audiostreams = { }
+	local nStreams = tonumber ( formatContext.nb_streams )
+	for i=0 , nStreams-1 do
+		local ctx = formatContext.streams [ i ].codec
+		if ctx.codec_type == avutil.AVMEDIA_TYPE_AUDIO then
+			local codec = assert ( avcodec.avcodec_find_decoder(ctx.codec_id) , "Unsupported codec" )
+			avAssert ( avcodec.avcodec_open ( ctx , codec ) )
+			tblinsert ( audiostreams , ctx )
+		end
+	end
+
+	return audiostreams
+end
+
+local packet = ffi.new("AVPacket")
+function ffmpeg.read_frames ( formatctx )
+	return function ( formatctx , packet )
+			if tonumber( avformat.url_feof ( formatctx.pb ) ) == 0 then
+				avAssert ( avformat.av_read_frame ( formatctx , packet ) )
+				return packet
+			else
+				return nil
+			end
+		end , formatctx.context , packet
+end
+
+avcodec.avcodec_init()
+avcodec.avcodec_register_all()
+avformat.av_register_all()
+
+return ffmpeg
